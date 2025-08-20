06d6177e35409cfeb337f092edae4757a40b82ba
Gleb Alshanskiy <rgcs.rgcs@gmail.com>
2025-08-20 20:11:16 +0300

    feat: add user filter by username, email

--- app/main.py
+++ app/main.py
@@ -13,16 +13,33 @@ else:
     app.debug = False
 
 
+users = {
+    1: {"username": "john_doe", "email": "john@example.com"},
+    2: {"username": "jane_doe", "email": "jane@example.com"},
+    3: {"username": "alice_jones", "email": "alice@example.com"},
+    4: {"username": "bob_white", "email": "bob@example.com"},
+}
+
+
 @app.get("/")
 def read_root():
     logger.info("Обработка рут гет запроса")
     return {"message": "Hello, World!"}
 
 
-@app.get("/users")
-def get_users():
-    logger.info("Получаем список юзеров")
-    return {"user": "yes"}
+@app.get("/users/")
+def read_users(username: str | None = None,
+               email: str | None = None,
+               limit: int = 10):
+    filtered_users = users
+
+    if username:
+        filtered_users = {key: user for key, user in filtered_users.items()
+                          if username.lower() in user["username"].lower()}
+    if email:
+        filtered_users = {key: user for key, user in filtered_users.items()
+                          if email.lower() in user["email"].lower()}
+    return dict(list(filtered_users.items())[:limit])
 
 
 @app.post("/add_user")
@@ -40,10 +57,9 @@ def get_db_info():
 @app.get("/users/{user_id}")
 async def get_user(user_id: int):
     logger.info("Ищем пользователя с id=%s", user_id)
-    return {
-        "user_id": user_id,
-        "user_info": f"Информация о пользователе с id={user_id}",
-    }
+    if user_id in users:
+        return users[user_id]
+    return {"error": "user not found"}
 
 
 @app.delete('/delete_user/{user_id}')
